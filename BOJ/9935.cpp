a147d6aff5352ba27ad19fb913269e574f11fb93
gyeongho-cho <gyeongh.cho@gmail.com>
2025-04-11 16:21:50 +0900

    solved BOJ 9935

--- BOJ/9935.cpp
+++ BOJ/9935.cpp
@@ -1,36 +1,40 @@
 #include <iostream>
 #include <string>
-#include <map>
+#include <vector>
 
 using namespace std;
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-    string s, s_query;
-    map<char, int> s_map;
-
-    cin >> s;
+    string s_query, s_key;
+    vector<char> s_stack;
     cin >> s_query;
-    for (int i=0; i<(s.length()-s_query.length()+1); i++){
-        int j = 0;
+    cin >> s_key;
+    for (int j=0; j<s_query.length(); j++){
+        s_stack.push_back(s_query[j]);
         bool boom_flag = true;
-        while (j < s_query.length()){
-            if(s[i+j] != s_query[j]){
-                boom_flag = false;
+        for (int i=0; i<s_key.length(); i++){
+            if (s_stack[s_stack.size()-1-i] != s_key[s_key.length()-i-1]){
+                boom_flag=false;
                 break;
             }
-            j++;
         }
-
-        if (!boom_flag){
-            cout << s[i];
-        }else{
-            i+= s_query.length();
+        if (boom_flag){
+            for (int i=0; i<s_key.length(); i++){
+                s_stack.pop_back();
+            }
         }
-
+        
+    }
+    if (s_stack.size()==0){
+        cout << "FRULA";
+    }
+    else{
+        for (char c : s_stack){
+            cout << c ;
+        }
     }
 
-
-    return 0;
+   return 0;
 }
